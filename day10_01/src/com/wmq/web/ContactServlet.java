a2504fa2c44974af1940af2389f8f1f01876db28
wang <bbxiong666@gmail.com>
2020-07-06 12:13:25 +0800

    合并servlet

---
+++ day10_01/src/com/wmq/web/ContactServlet.java
@@ -0,0 +1,124 @@
+package com.wmq.web;
+
+import com.wmq.pojo.Contact;
+import com.wmq.pojo.PageBean;
+import com.wmq.service.ContactService;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+@WebServlet("/contact")
+public class ContactServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if("findContactAll".equals(action)){
+            findContactAll(request,response);
+        }else if("addContact".equals(action)){
+            addContact(request,response);
+        }else if("findContactById".equals(action)){
+            findContactById(request,response);
+        }else if("updateContact".equals(action)){
+            updateContact(request,response);
+        }else if("deleteContact".equals(action)){
+            deleteContactById(request,response);
+        }else if("findContactByPage".equals(action)){
+            findContactByPage(request,response);
+        }
+
+    }
+
+    private void findContactByPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int pageSize = 3;
+        int pageNumber = 1;
+        pageSize = Integer.parseInt(request.getParameter("pageSize"));
+        pageNumber = Integer.parseInt(request.getParameter("pageNumber"));
+        ContactService contactService = new ContactService();
+        PageBean contactByPage = contactService.findContactByPage(pageSize, pageNumber);
+        request.setAttribute("pb",contactByPage);
+        request.getRequestDispatcher("pageList.jsp").forward(request,response);
+    }
+
+    private void deleteContactById(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String id = request.getParameter("id");
+        ContactService contactService = new ContactService();
+        boolean b = contactService.deleteContact(id);
+        //当删除成功后跳转到展示页面
+        if (b) {
+            response.sendRedirect(request.getContextPath() + "/contact?action=findContactByPage&pageNumber=1&pageSize=3");
+        } else {
+            response.sendRedirect(request.getContextPath() + "/error.jsp");
+        }
+    }
+
+    private void updateContact(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Map<String, String[]> map = request.getParameterMap();
+            Contact contact = new Contact();
+            BeanUtils.populate(contact, map);
+            ContactService service = new ContactService();
+            boolean flag = service.updateContact(contact);
+            //判断是否有数据，有数据就返回展示，没有跳转到错误界面
+            if (flag) {
+                response.sendRedirect(request.getContextPath() + "/contact?action=findContactByPage&pageNumber=1&pageSize=3");
+            } else {
+                response.sendRedirect(request.getContextPath() + "/error.jsp");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void findContactById(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        ContactService contactService = new ContactService();
+        Contact contactById = contactService.findContactById(id);
+        //将查询到的数据放入共享区并请求转发到修改界面
+        request.setAttribute("contact",contactById);
+        request.getRequestDispatcher("/update.jsp").forward(request,response );
+    }
+
+    private void addContact(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Map<String, String[]> parameterMap = request.getParameterMap();
+            Contact contact = new Contact();
+            BeanUtils.populate(contact,parameterMap);
+            ContactService contactService = new ContactService();
+            boolean flag = contactService.addContact(contact);
+            //判断是否有数据，有数据重定向到查询所有页面展示
+            /**
+             * 为什么用重定向不用请求转发：
+             *        重定向的路径会改变，不然刷新会重新执行一次
+             */
+            if (flag) {
+                response.sendRedirect(request.getContextPath() + "/contact?action=findContactByPage&pageNumber=1&pageSize=3");
+            } else {
+                response.sendRedirect(request.getContextPath()+"/error.jsp");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void findContactAll(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //调用service处理业务逻辑
+        ContactService service = new ContactService();
+        List<Contact> list = service.findContactAll();
+        request.setAttribute("list", list);
+        request.getRequestDispatcher("list.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
---
+++ day10_01/web/error.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>你输入的信息有误...</h1>
+</body>
+</html>
